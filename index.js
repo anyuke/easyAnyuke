9826f21676a212c585881a02a1a44936e96cd0d3
liyunfei <1249836965@qq.com>
2017-02-27 21:11:53 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var transform = function(key, callback) {
+    var newArr = [];
+    var data = [];
+    newArr.push(arr[0]);
+    data.push(newArr);
+    for (var i = 1; i < arr.length; i++) {
+        var flag = 1;
+        var row = arr[i];
+        for (var j = 0; j < data.length; j++) {
+            for (var k = 0; k < data[j].length; k++) {
+                if (row[key] == data[j][k][key]) {
+                    data[j].push(row);
+                    flag = 0;
+                    break;
+                }
+            }
+        }
+        if (flag) {
+            var temp = [];
+            temp.push(row);
+            data.push(temp);
+        }
+    }
+    callback(data);
+}
+
+module.exports = transform;
